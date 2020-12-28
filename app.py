920e391f26f46e16b9fe6e455256b7c902324fd0
Victor Bilgin <services@victorbilgin.com>
2020-12-28 05:58:46 +0000

    Final prep

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
 
 client = WebClient(token=os.environ.get('SLACK_BOT_TOKEN'))
-channel_id = "C01J157L8QH"
+channel_id = "C01H85G2VV5"
 
 def get_rand_poem():
   res = requests.get('https://poetrydb.org/random')
@@ -14,18 +14,17 @@ def get_rand_poem():
 
 
 try:
-    # poem = get_rand_poem()
-    # poem_title = poem['title']
-    # poem_author = poem['author']
-    # poem_text = ''
-    # for line in poem['lines']:
-    #   poem_text = poem_text + line + '\n'
+    poem = get_rand_poem()
+    poem_title = poem['title']
+    poem_author = poem['author']
+    poem_text = ''
+    for line in poem['lines']:
+      poem_text = poem_text + line + '\n'
 
     # Call the chat.postMessage method using the WebClient
     result = client.chat_postMessage(
         channel= channel_id, 
-        # text= f'*Title*: {poem_title}\n*Author*: {poem_author}\n\n {poem_text}'
-        text= "Testing"
+        text= f'*Title*: {poem_title}\n*Author*: {poem_author}\n\n {poem_text}'
     )
 
 except SlackApiError as e:
